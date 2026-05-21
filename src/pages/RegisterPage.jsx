5af25350fe9907ba13cab53b6194c871843767ce
RezaR2P <kecilreza575@gmail.com>
2026-05-21 16:41:40 +0700

    membuat halaman register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,28 +2,46 @@ import { useState } from 'react';
 import api from '../services/api';
 import { useNavigate } from 'react-router-dom';
 
-const LoginPage = () => {
+const RegisterPage = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // console.log(name, email, password, phone);
     try {
-      const response = await api.post('/auth/login', { email, password });
-      // console.log(response.data);
-      localStorage.setItem('token', response.data.data);
-      navigate('/');
+      const response = await api.post('/auth/register', {
+        name,
+        email,
+        password,
+        phone,
+      });
+      console.log(response.data);
+      navigate('/login');
     } catch (error) {
-      console.log(error);
+      console.log(error.response.data);
     }
-    // console.log(email, password);
   };
 
   return (
     <div className="flex item-center justify-center h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-96">
         <h1 className="text-2xl font-bold text-center mb-6">Login</h1>
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2">Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Masukkan Nama"
+          />
+        </div>
         <div className="mb-4">
           <label className="block text-gray-700 mb-2">Email</label>
           <input
@@ -48,6 +66,18 @@ const LoginPage = () => {
             placeholder="Masukkan Password"
           />
         </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2">No HP</label>
+          <input
+            type="tel"
+            value={phone}
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+            className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Masukkan Nomor HP"
+          />
+        </div>
         <button
           onClick={handleSubmit}
           className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
@@ -59,4 +89,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
