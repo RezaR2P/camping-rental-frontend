2a2233fedf803db7d2d93a250c35e2a1be04b68a
RezaR2P <kecilreza575@gmail.com>
2026-05-22 13:27:45 +0700

    menambahkan protected route, jadi hanya bisa masuk ke halaman home ketika sudah login atau sudah mendapatkan token

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
